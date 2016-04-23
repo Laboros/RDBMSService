32a9234f2d13d24d7a68e7b0015400a559e9981a
naveenkannam <naveen.ketu@gmail.com>
2016-04-23 20:32:38 +0530

    Created pojo classes persistence classes

---
+++ RDBMSService/src/main/java/com/vodafone/rdbms/pojo/Employee.java
@@ -0,0 +1,21 @@
+package com.vodafone.rdbms.pojo;
+
+public class Employee {
+	
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer eno) {
+		this.id = eno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
