292cebaa6b0627d3f3c14faf73630cc44936cda1
svaduka <sainagaraju.vaduka@gmail.com>
2016-07-10 17:10:33 +0530

    TestHibernate

--- RDBMSService/src/main/java/com/vodafone/rdbms/dao/TestHibernate.java
+++ RDBMSService/src/main/java/com/vodafone/rdbms/dao/TestHibernate.java
@@ -1,5 +1,9 @@
 package com.vodafone.rdbms.dao;
 
+import java.sql.Date;
+
+import com.vodafone.rdbms.pojo.Employee;
+
 public class TestHibernate {
 
 	/**
@@ -8,6 +12,10 @@ public class TestHibernate {
 	public static void main(String[] args) {
 
 //		Hiber
+		
+		System.out.println("******* WRITE *******");
+		Employee empl = new Employee("Jack", "Bauer", new Date(System.currentTimeMillis()), "911");
+		HibernateDAO.save(empl);
 	}
 
 }
